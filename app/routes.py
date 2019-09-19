00be4ef2acac2d488fa1573948817bc182718649
DataByne <databyne@gmail.com>
2019-09-19 02:03:01 -0400

    Typo

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,5 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'usernmae': 'Dory'}
+    user = {'username': 'Dory'}
     return render_template( 'index.html', title='Home', user=user )
